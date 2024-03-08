2d7a8ad80c954ec7fcfae891320f02dfa31f66fc
Sarahaha004 <211001004@rajalakshmi.edu.in>
2024-03-08 23:13:02 +0530

    Floyd cycle algo

---
+++ CycleLinkList.java
@@ -0,0 +1,33 @@
+/**
+ * Find if Cycle in Link List
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null)
+        {
+            return false;
+        }
+        ListNode tort=head;
+        ListNode hare=head.next;
+
+        while(hare!=null && hare.next!=null)
+        {
+            if(tort==hare)
+            {
+                return true;
+            }
+            tort=tort.next;
+            hare=hare.next.next;
+        }
+        return false;
+    }
+}
